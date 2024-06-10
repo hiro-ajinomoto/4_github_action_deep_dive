bc7eb35e003acc77e51df42c9be1e23a3721f783
adam <dathoang9635@gmail.com>
2024-06-10 17:39:33 +0700

    added comment

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -2,10 +2,12 @@ import { string } from 'prop-types';
 
 import './HelpBox.css';
 
+
+// My test
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h2>{title}</h2>
+      <h3>{title}</h3>
       <p>{text}</p>
     </article>
   );
